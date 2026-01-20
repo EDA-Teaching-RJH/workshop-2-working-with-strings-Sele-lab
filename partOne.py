1f0befa23531442ec18f03b0dda15f4e7468cf1b
Sele-lab <ee14@kent.ac.uk>
2026-01-20 12:46:01 +0000

    Workshop 2

--- partOne.py
+++ partOne.py
@@ -3,7 +3,6 @@ def main():
     myFunction(slow)
 
 def myFunction(text):
-  b = input("What was your fondest memory?" )#Your code goes here.
-  c=b.replace(' ', 'a')
-
+    c = text.replace(' ', '.')
+    print(c)
 main()
