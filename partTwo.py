cee796b4c62aa45e9322ec5cf9a2256076a7d5fd
Sele-lab <ee14@kent.ac.uk>
2026-01-20 13:04:31 +0000

    Workshop 2

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,11 @@
 import math  
 
 def main():
-#TO DO  
+    A = int(input("What is A ?"))#TO DO 
+    B = int(input("What is B ?")) 
+    print("C is", pythag(A,B))
 
 def pythag(A,B):
-#TO DO  
+    return (math.sqrt(A**2 + B**2)) #TO DO  
 
 main()
